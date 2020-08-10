ba25c6301e8aa83132eb5f4093fba87b6f0a849c
Aveline Wingfield <avelinewingfield@gmail.com>
2020-08-10 16:22:31 -0400

    Everything installed. Store connected and functional.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
 import App from './App';
+import carreducer from './reducers/carreducer'
 
 import 'bulma/css/bulma.css';
 import './styles.scss';
 
+const store = createStore(carreducer)
+
 const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(<Provider store = {store}><App /></Provider>, rootElement);
